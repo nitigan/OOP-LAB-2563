7943fceae2cd47644c589a492b927b4b56a9d297
nitigan.n <nitigan.n@fitm.kmutnb.ac.th>
2021-02-10 00:31:54 +0700

    exam

--- testOOP/src/test2.java
+++ testOOP/src/test2.java
@@ -1,74 +1,84 @@
 package testOOP.src;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class test2 {
-    static String userArray[][];
+    static String userArray[][],output="";
     static Scanner scan = new Scanner(System.in);
+    static boolean chkUser=false,chkPass=false;
     public static void main(String[] args) {
-
-        System.out.println("Create Username and Password");
+        JOptionPane.showMessageDialog(null, "Welcome to KMUTNB", "KLOGIC", JOptionPane.INFORMATION_MESSAGE);
+        //System.out.println("Create Username and Password");
         createUser();
         showUser();
-        loginTest();
+        do{
+            loginTest();
+        }while(!chkUser || !chkPass);
         scan.close();
     }
 
     public static void createUser(){
         //Scanner scan = new Scanner(System.in);
         int userNum=0;
+        String userNumString;
         
-        System.out.print("How many User : ");
-        userNum = scan.nextInt();
+        //System.out.print("How many User : ");
+        //userNum = scan.nextInt();
+        userNumString = JOptionPane.showInputDialog(null,"How many User :","Create User",JOptionPane.QUESTION_MESSAGE);
+        userNum = Integer.parseInt(userNumString);
         userArray = new String[userNum][2];
         
         for(int i = 0 ; i < userArray.length; i++){
-            System.out.print("Enter Username for User:["+(i+1)+"] : " );
+            /*System.out.print("Enter Username for User:["+(i+1)+"] : " );
             userArray[i][0] = scan.next().toString();
             System.out.print("Enter Password for User:["+(i+1)+"] : " );
             userArray[i][1] = scan.next().toString();
+            */
+            userArray[i][0] = JOptionPane.showInputDialog("Enter USERNAME for User:["+(i+1)+"] : ");
+            userArray[i][1] = JOptionPane.showInputDialog("Enter PASSWORD for User:["+(i+1)+"] : ");
         }    
-        
     }
 
     public static void showUser() {
         for(int i = 0 ; i < userArray.length; i++){
-            System.out.print("User["+(i+1) +"] : User is " +userArray[i][0]);
-            System.out.println(" : Password is " +userArray[i][1]);
+           output += "User["+(i+1) +"] : User is " +userArray[i][0];
+           output += " : Password is " +userArray[i][1]+"\n";
         }
+        JOptionPane.showMessageDialog(null, output, "Show Username and Password", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static void loginTest(){
         String userName,passWord;
-        boolean chkUser=false,chkPass=false;
-        System.out.println("Login");
+        
+        //System.out.println("Login");
 
-        System.out.print("Enter Username : ");
-        userName = scan.next().toString();
+        //System.out.print("Enter Username : ");
 
-        System.out.print("Enter Password : ");
-        passWord = scan.next().toString();
+        userName = JOptionPane.showInputDialog(null,"Username:","Login",JOptionPane.QUESTION_MESSAGE);
+        passWord = JOptionPane.showInputDialog(null,"Password:","Login",JOptionPane.QUESTION_MESSAGE);
+        //System.out.print("Enter Password : ");
+        //passWord = scan.next().toString();
 
+        
         for(int j = 0 ; j <userArray.length ; j++){
-
-            //if(userName.equals(userArray[j][0]) && passWord.equals(userArray[j][1])){
             if(userName.equals(userArray[j][0])){
                 chkUser = true;
                 if(passWord.equals(userArray[j][1])){
-                    System.out.println("Login Success");
+                    //System.out.println("Login Success");
+                    chkPass = true; 
+                    JOptionPane.showMessageDialog(null, "Login Success\n Hello "+userName, "Welcome to KLOGIC", JOptionPane.INFORMATION_MESSAGE);
                 }else{
-                    System.out.println("Invalid Password");
+                    JOptionPane.showMessageDialog(null, "Invalid Password", "You are can't login", JOptionPane.ERROR_MESSAGE);
+                    //System.out.println("Invalid Password");
                 }
                 break;
             }else{
-                chkUser = false;
-               
+                chkUser = false;               
             }
         }
         if(!chkUser){
-            System.out.println("Invalid Username");
+            //System.out.println("Invalid Username");
+            JOptionPane.showMessageDialog(null, "Invalid Username", "You are can't login", JOptionPane.ERROR_MESSAGE);
         }
-
-
     }
-
 }

251cc97a9f70ca15733a8baddc5b7e45d31318a6
Nitigan Nakjuatong <nitigan.n@fitm.kmutnb.ac.th>
2021-02-23 10:27:18 +0700

    Add files via upload
    chapter6

---
+++ chapter6/src/lab6_2.java
@@ -0,0 +1,39 @@
+package chapter6.src;
+import javax.swing.JOptionPane;
+
+public class lab6_2 {
+    public static void main(String[] args) {
+        int score[] = new int[10]; // declare and allocate array
+        String output = "";
+
+        //initial value
+        for(int n = 0 ; n < score.length ; n++)
+            score[n] = rnd(100,500);
+        for(int n = 0; n < score.length ; n++)
+            output += "Score " + (n+1) + " : " +score[n] + "\n";
+        output += "Minimum value is " + getMin(score) + "\n";
+        output += "Maximum value is " + getMax(score) + "\n";
+        output += "Average value is " + getAverage(score) + "\n";
+        
+        JOptionPane.showMessageDialog( null, output ,
+        "Display Score in Array", JOptionPane.INFORMATION_MESSAGE );
+    }
+
+    public static int getMin(int data[]) {
+        return (0);
+    }
+
+    public static int getMax(int data[]) {
+        return (0);
+    }
+
+    public static int getAverage(int data[]) {
+        return (0);
+    }
+
+    public static int rnd(int st, int ed) {
+        int offset = ed - st +1;
+        return (st + (int) (Math.random() * offset));
+    } 
+
+}

dcd6c7eea4541003894b73105d6cbd3c6a5ffbb1
nitigan.n <nitigan.n@fitm.kmutnb.ac.th>
2021-03-12 09:57:05 +0700

    lab7

--- Chapter7/Lab7_1.java
+++ Chapter7/Lab7_1.java
@@ -3,9 +3,10 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 public class Lab7_1 extends JFrame{
-JLabel numberLabel, resultLabel;
-JTextField numberField;
+JLabel numberLabel, resultLabel,blankLabel;
+JTextField numberField,blankField;
 JTextArea resultArea;
+
 public Lab7_1() {
     // title bar in window
     super("Program display value n");
@@ -13,40 +14,51 @@ public Lab7_1() {
     Container container = getContentPane();
     container.setLayout( new FlowLayout() );
     // create numberLabel and attach it to content pane
-    numberLabel = new JLabel("Enter integer and press Enter");
+    numberLabel = new JLabel("Enter integer and press Enter : ");
     container.add( numberLabel );
     // create numberField and attach it to content pane
     numberField = new JTextField( 10 );
     container.add( numberField );
     // register this applet as numberFields ActionListener
-    numberField.addActionListener(
-    // create inner class
-    new ActionListener() {
-        public void actionPerformed( ActionEvent event )
-        {
-            int number, sumValue;
-            String blank5 = " ";
-            number = Integer.parseInt( numberField.getText() );
-            // clear value in TextArea
-            resultArea.setText("");
-            // add data in textarea
-            for (int n = 1 ; n <= number ; n++) {
-                resultArea.append( blank5 + Integer.toString(n));
-                if (n % 5 == 0) resultArea.append("\n");
-            }
-            // clear value in numberField
-            numberField.setText("");
-        } // end method actionPerformed
-    }
+
+    blankLabel = new JLabel("Enter integer number of blank : ");
+    container.add( blankLabel );
+    // create numberField and attach it to content pane
+    blankField = new JTextField( 10 );
+    container.add( blankField );
+
+    blankField.addActionListener(
+        // create inner class
+        new ActionListener() {
+            public void actionPerformed( ActionEvent event )
+            {
+                int number, sumValue, blankNum;
+                String blank = " ";
+                number = Integer.parseInt( numberField.getText() );
+                blankNum = Integer.parseInt(blankField.getText() ); 
+                for(int j=1;j<=blankNum;j++){
+                    blank +=" ";
+                }
+                // clear value in TextArea
+                resultArea.setText("");
+                // add data in textarea
+                for (int n = 1 ; n <= number ; n++) {
+                    resultArea.append( blank + Integer.toString(n));
+                    if (n % 5 == 0) resultArea.append("\n");
+                }
+                // clear value in numberField
+                numberField.setText("");
+            } // end method actionPerformed
+        }
     );
     // create display
-    resultArea = new JTextArea( 10,20 );
+    resultArea = new JTextArea( 20,30 );
     resultArea.setEditable( false );
     container.add( resultArea );
 }
     public static void main(String[] args) {
         Lab7_1 window = new Lab7_1();
-        window.setSize( 360,240);
+        window.setSize( 460,440);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
     }
